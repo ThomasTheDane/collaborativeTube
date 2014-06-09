f4ec61535daef176053fc5435e099ea18c1427dd
Nattestad, Thomas <Thomas_Nattestad@intuit.com>
2014-06-08 20:55:10 -0700

    removed some account stuff from views

--- controllers/contact.js
+++ controllers/contact.js
@@ -27,37 +27,37 @@ exports.getContact = function(req, res) {
  * @param message
  */
 
-exports.postContact = function(req, res) {
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('message', 'Message cannot be blank').notEmpty();
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/contact');
-  }
-
-  var from = req.body.email;
-  var name = req.body.name;
-  var body = req.body.message;
-  var to = 'your@email.com';
-  var subject = 'Contact Form | Hackathon Starter';
-
-  var mailOptions = {
-    to: to,
-    from: from,
-    subject: subject,
-    text: body
-  };
-
-//  smtpTransport.sendMail(mailOptions, function(err) {
-//    if (err) {
-//      req.flash('errors', { msg: err.message });
-//      return res.redirect('/contact');
-//    }
-//    req.flash('success', { msg: 'Email has been sent successfully!' });
-//    res.redirect('/contact');
-//  });
-};
+//exports.postContact = function(req, res) {
+//  req.assert('name', 'Name cannot be blank').notEmpty();
+//  req.assert('email', 'Email is not valid').isEmail();
+//  req.assert('message', 'Message cannot be blank').notEmpty();
+//
+//  var errors = req.validationErrors();
+//
+//  if (errors) {
+//    req.flash('errors', errors);
+//    return res.redirect('/contact');
+//  }
+//
+//  var from = req.body.email;
+//  var name = req.body.name;
+//  var body = req.body.message;
+//  var to = 'your@email.com';
+//  var subject = 'Contact Form | Hackathon Starter';
+//
+//  var mailOptions = {
+//    to: to,
+//    from: from,
+//    subject: subject,
+//    text: body
+//  };
+//
+////  smtpTransport.sendMail(mailOptions, function(err) {
+////    if (err) {
+////      req.flash('errors', { msg: err.message });
+////      return res.redirect('/contact');
+////    }
+////    req.flash('success', { msg: 'Email has been sent successfully!' });
+////    res.redirect('/contact');
+////  });
+//};
