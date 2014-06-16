99865da1924344c44259294fee084b9160cfbb44
Nattestad, Thomas <Thomas_Nattestad@intuit.com>
2014-06-16 09:38:48 -0700

    Excellent progress on room setup and creation server side

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
 
 app.get('/', homeController.index);
 app.get('/newRoom', newRoomController.newRoom);
+app.post('/newRoom', newRoomController.postNewRoom);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
--- controllers/newRoom.js
+++ controllers/newRoom.js
@@ -1,4 +1,5 @@
 var secrets = require('../config/secrets');
+var Firebase = require('firebase');
 //var nodemailer = require("nodemailer");
 //var smtpTransport = nodemailer.createTransport('SMTP', {
 //  service: 'SendGrid',
@@ -16,6 +17,50 @@ var secrets = require('../config/secrets');
 exports.newRoom = function(req, res) {
   res.render('newRoom', {
     title: 'New Room'
+  });
+};
+
+exports.postNewRoom = function(req, res){
+  req.assert('roomName', 'Name cannot be blank').notEmpty();
+
+  var name = req.body.roomName;
+  var adminPass = req.body.adminPass;
+  var description = req.body.roomDescription;
+  var publicSeeName = req.body.publicSeeName;
+  var publicCanAdd = req.body.publicCanAdd;
+  var publicCanDelete = req.body.publicCanDelete;
+  console.log(name + " " + description + " " + publicSeeName + " " + adminPass+" " +publicCanAdd+" " +publicCanDelete);
+
+  var roomsRef = new Firebase('https://ourtube.firebaseIO.com/rooms/' + name);
+  //listen to see if room already exists
+  roomsRef.once('value', function(data){
+    console.log('pong off firebase');
+    if(data.val() == null){
+      roomsRef.set({description: description, date: (new Date).toDateString(), time: (new Date).toTimeString()});
+    }else{
+      console.log('room already exists');
+      req.assert('', 'Room already exists, please pick a different name').notEmpty();
+    }
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      req.flash('errors', errors);
+      return res.redirect('/newRoom');
+    }
+
+    console.log('redirecting after creating room');
+    return res.redirect('/room/'+name);
+
+  }, function(err){
+    req.assert('', 'There was a problem connecting and the room was not created. Our deepest apologies, please try again later').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      req.flash('errors', errors);
+      return res.redirect('/newRoom');
+    }
   });
 };
 
